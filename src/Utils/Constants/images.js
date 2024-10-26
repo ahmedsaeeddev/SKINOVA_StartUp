2c65f5c8b546fcd02758ec26ad777ac5c5dab344
ahmedsaeeddev <ahmedsaeeddev0@gmail.com>
2024-10-26 21:45:33 +0500

    first commit

--- src/Utils/Constants/images.js
+++ src/Utils/Constants/images.js
@@ -1,22 +1,22 @@
 export let images = [
     {
         name: 'logo',
-        src: '../../src/assets/img/logo.png',
+        src: '../../../assets/img/logo.png',
     },
     {
         name: 'laptop_banner',
-        src: '../../src/assets/img/laptop_banner.jpg',
+        src: '../../../assets/img/laptop_banner.jpg',
     },
     {
         name: 'anime_laptop',
-        src: '../../src/assets/img/anime_laptop.jpg',
+        src: '../../../assets/img/anime_laptop.jpg',
     },
     {
         name: 'mobile',
-        src: '../../src/assets/img/mobile.jpg',
+        src: '../../../assets/img/mobile.jpg',
     },
     {
         name: 'custom_laptop',
-        src: '../../src/assets/img/custom_skins.png',
+        src: '../../../assets/img/custom_skins.png',
     },
 ]
